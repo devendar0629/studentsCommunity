f146db195b8ff745dd79aa1295bf5d94cdddc9aa
devendar0629 <devendar0629@gmail.com>
2024-05-26 17:40:01 +0530

    add svg and refactor signup success page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,9 @@ import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Loader from "@/components/custom/Loader/Loader";
-import { useAppSelector, useAppDispatch } from "@/redux/hooks/hooks";
+import { useAppDispatch } from "@/redux/hooks/hooks";
+import lockSVG from "../../../public/lock.svg";
+import usernameSVG from "../../../public/username.svg";
 import { login } from "@/redux/features/auth/authSlice";
 
 interface FormInputs {
@@ -82,16 +84,11 @@ function Login() {
                                     placeholder="Email or username"
                                     type="text"
                                 />
-                                <svg
-                                    fill="#000000"
-                                    width="1.35rem"
-                                    height="40px"
-                                    className="absolute left-3"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M12,1a11,11,0,0,0,0,22,1,1,0,0,0,0-2,9,9,0,1,1,9-9v2.857a1.857,1.857,0,0,1-3.714,0V7.714a1,1,0,1,0-2,0v.179A5.234,5.234,0,0,0,12,6.714a5.286,5.286,0,1,0,3.465,9.245A3.847,3.847,0,0,0,23,14.857V12A11.013,11.013,0,0,0,12,1Zm0,14.286A3.286,3.286,0,1,1,15.286,12,3.29,3.29,0,0,1,12,15.286Z" />
-                                </svg>
+                                <img
+                                    className="absolute left-3 h-[40px] w-[1.35rem]"
+                                    src={usernameSVG.src}
+                                    alt=""
+                                />
                             </section>
                             {errors.username_or_email && (
                                 <div className="text-red-300 text-sm font-light text-center">
@@ -116,22 +113,11 @@ function Login() {
                                     placeholder="Password"
                                     type="password"
                                 />
-                                <svg
-                                    width="1.425rem"
-                                    height="1.45rem"
-                                    viewBox="0 0 24 24"
-                                    className="absolute left-3"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288"
-                                        stroke="#000000"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    />
-                                </svg>
+                                <img
+                                    className="absolute left-3 h-[1.45rem] w-[1.425rem]x"
+                                    src={lockSVG.src}
+                                    alt=""
+                                />
                             </section>
 
                             {errors.password && (
